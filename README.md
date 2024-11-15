0af826eef27064529c70bceaccb94f867b3d6589
unblocktechie <unblocktechie@gmail.com>
2024-11-15 19:35:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,212 @@
+# RWA Vault #
+
+## Table of Contents
+* [Frameworks and Tooling](#frameworks-and-tooling)
+* [Repository Description](#repository-description)
+* [Installation](#installation)
+* [Configuration](#configuration)
+* [Alternative Configuration: Using Private Keys instead of Mnemonics, and Alchemy instead of Infura](#alternative-configuration-using-private-keys-instead-of-mnemonics-and-alchemy-instead-of-infura)
+* [Using Custom JSON-RPC Endpoint URL](#using-custom-json-rpc-endpoint-url)
+* [Testing](#testing)
+* [Test Coverage](#test-coverage)
+* [Deployment](#deployment)
+* [Contributing](#contributing)
+
+## Frameworks and Tooling
+The project is built using
+* [Hardhat](https://hardhat.org/), a popular Ethereum development environment
+    * Why not standalone [Truffle](https://www.trufflesuite.com/truffle)?
+        * Truffle runs the tests several times slower than Hardhat
+        * Truffle + [ganache](https://trufflesuite.com/ganache/) fails to run big test suites,
+        presumably it fails to close socket connections gracefully and causes open sockets overflow
+    * Why not [Foundry](https://github.com/foundry-rs/foundry)?
+        * Foundry forces the tests to be written in Solidity, which complicates
+            * porting the existing tests from myriad of projects using JavaScript for tests,
+            * getting help from the vast community of [Node.js](https://nodejs.org/en) developers in writing tests
+* [Web3.js](https://web3js.readthedocs.io/), a collection of libraries that allows interacting with
+local or remote Ethereum node using HTTP, IPC or WebSocket
+* [Truffle](https://www.trufflesuite.com/truffle) as a Hardhat module (plugin)
+
+Smart contracts deployment is configured to use [Infura](https://infura.io/) or [Alchemy](https://www.alchemy.com/)
+and [HD Wallet](https://www.npmjs.com/package/@truffle/hdwallet-provider)
+
+## Installation ##
+
+Following steps were tested to work in macOS Catalina
+
+1. Install [Node Version Manager (nvm)](https://github.com/nvm-sh/nvm) – latest  
+    ```brew install nvm```
+2. Install [Node package manager (npm)](https://www.npmjs.com/) and [Node.js](https://nodejs.org/) – version 16.20.0+  
+    ```nvm install 16```
+3. Activate node version installed  
+    ```nvm use 16```
+4. Install project dependencies  
+    ```npm install```
+
+### Notes on Ubuntu 20.04 LTS ###
+- [How to install Node.js 16 on Ubuntu 20.04 LTS](https://joshtronic.com/2021/05/09/how-to-install-nodejs-16-on-ubuntu-2004-lts/)
+- [How to Run Linux Commands in Background](https://linuxize.com/post/how-to-run-linux-commands-in-background/)
+
+## Configuration ##
+1.  Create or import 12-word mnemonics for
+    1. Mainnet
+    2. Mantle
+    3. Linea
+    4. Unichain
+    5. Zircuit
+
+    You can use MetaMask to create mnemonics: https://metamask.io/
+
+    > Note: you can use same mnemonic for test networks.
+    Always use a separate one for mainnet, keep it secure.
+
+    > Note: you can add more configurations to connect to the networks not listed above.
+    Check and add configurations required into the [hardhat.config.js](hardhat.config.js).
+
+    > Note: you can use private keys instead of mnemonics (see Alternative Configuration section below)
+
+2.  Create an infura access key at https://infura.io/
+
+    Note: you can use alchemy API key instead of infura access key (see Alternative Configuration section below)
+
+3.  Create etherscan API key at https://etherscan.io/
+
+4.  Export mnemonics, infura access key, and etherscan API key as system environment variables
+    (they should be available for hardhat):
+
+    | Name                 | Value                        |
+    |----------------------|------------------------------|
+    | MNEMONIC1            | Mainnet mnemonic             |
+    | MNEMONIC5003         | Mantle testnet mnemonic      |
+    | MNEMONIC59141        | Linea testnet mnemonic       |
+    | MNEMONIC1301         | Unichain testnet mnemonic    |
+    | MNEMONIC48899        | Zircuit testnet mnemonic     |
+    | INFURA_KEY           | Infura access key            |
+    | ETHERSCAN_KEY        | Etherscan API key            |
+    | ARBISCAN_KEY         | arbiscan API key             |
+    
+> Note:  
+Read [How do I set an environment variable?](https://www.schrodinger.com/kb/1842) article for more info on how to
+set up environment variables in Linux, Windows and macOS.
+
+### Example Script: macOS Catalina ###
+```
+export MNEMONIC1="witch collapse practice feed shame open despair creek road again ice least"
+export MNEMONIC5003="someone relief rubber remove donkey jazz segment nose spray century put beach"
+export MNEMONIC59141="slush oyster cash hotel choice universe puzzle slot reflect sword intact fat"
+export MNEMONIC1301="result mom hard lend adapt where result mule address ivory excuse embody"
+export INFURA_KEY="000ba27dfb1b3663aadfc74c3ab092ae"
+export ETHERSCAN_KEY="9GEEN6VPKUR7O6ZFBJEKCWSK49YGMPUBBG"
+export ARBISCAN_KEY="VF9IZLVDRA03VE3K5S46EADMW6VNV0V73U"
+```
+
+## Alternative Configuration: Using Private Keys instead of Mnemonics, and Alchemy instead of Infura ##
+Alternatively to using mnemonics, private keys can be used instead.
+When both mnemonics and private keys are set in the environment variables, private keys are used.
+
+Similarly, alchemy can be used instead of infura.
+If both infura and alchemy keys are set, alchemy is used.
+
+1.  Create or import private keys of the accounts for
+    1. Mainnet
+    2. Mantle
+    3. Linea
+    4. Unichain
+    5. Zircuit
+
+    You can use MetaMask to export private keys: https://metamask.io/
+
+2.  Create an alchemy API key at https://alchemy.com/
+
+3.  Create etherscan API key at https://etherscan.io/
+
+4.  Export private keys, infura access key, and etherscan API key as system environment variables
+    (they should be available for hardhat):
+
+    | Name          | Value                         |
+    |---------------|-------------------------------|
+    | P_KEY1        | Mainnet private key           |
+    | P_KEY5003     | Mantle testnet private key    |
+    | P_KEY59141    | Linea testnet private key     |
+    | P_KEY1301     | Unichain testnet private key  |
+    | P_KEY48899    | Zircuit testnet private key   |
+    | ALCHEMY_KEY   | Alchemy API key               |
+    | ETHERSCAN_KEY | Etherscan API key             |
+    | ARBISCAN_KEY  | arbiscan API key              |
+    
+> Note: private keys should start with ```0x```
+
+### Example Script: macOS Catalina ###
+```
+export P_KEY1="0x5ed21858f273023c7fc0683a1e853ec38636553203e531a79d677cb39b3d85ad"
+export P_KEY5003="0xfb84b845b8ea672939f5f6c9a43b2ae53b3ee5eb8480a4bfc5ceeefa459bf20c"
+export P_KEY59141="0x5ed21858f273023c7fc0683a1e853ec38636553203e531a79d677cb39b3d85ad"
+export P_KEY1301="0xfb84b845b8ea672939f5f6c9a43b2ae53b3ee5eb8480a4bfc5ceeefa459bf20c"
+export ALCHEMY_KEY="hLe1XqWAUlvmlW42Ka5fdgbpb97ENsMJ"
+export ETHERSCAN_KEY="9GEEN6VPKUR7O6ZFBJEKCWSK49YGMPUBBG"
+export ARBISCAN_KEY="VF9IZLVDRA03VE3K5S46EADMW6VNV0V73U"
+```
+
+## Using Custom JSON-RPC Endpoint URL ##
+To use custom JSON-RPC endpoint instead of infura/alchemy public endpoints, set the corresponding RPC URL as
+an environment variable:
+
+| Name                | Value                                   |
+|---------------------|-----------------------------------------|
+| MAINNET_RPC_URL     | Mainnet JSON-RPC endpoint URL           |
+| MANTLE_RPC_URL      | Mantle testnet JSON-RPC endpoint URL    |
+| LINEA_RPC_URL       | Linea testnet JSON-RPC endpoint URL     |
+| UNICHAIN_RPC_URL    | Unichain testnet JSON-RPC endpoint URL  |
+| ZIRCUIT_RPC_URL     | Zircuit testnet JSON-RPC endpoint URL   |
+
+## Compilation ##
+Execute ```npx hardhat compile``` command to compile smart contracts.
+
+Compilation settings are defined in [hardhat.config.js](./hardhat.config.js) ```solidity``` section.
+
+Note: Solidity files *.sol use strict compiler version, you need to change all the headers when upgrading the
+compiler to another version 
+
+## Testing ##
+Smart contract tests are built with Truffle – in JavaScript (ES6) and [web3.js](https://web3js.readthedocs.io/)
+
+The tests are located in [test](./test) folder. 
+They can be run with built-in [Hardhat Network](https://hardhat.org/hardhat-network/).
+
+Run ```npx hardhat test``` to run all the tests or ```.npx hardhat test <test_file>``` to run individual test file.
+
+## Test Coverage ##
+Smart contracts test coverage is powered by [solidity-coverage] plugin.
+
+Run `npx hardhat coverage` to run test coverage and generate the report.
+
+## Deployment ##
+Deployments are implemented via [hardhat-deploy plugin](https://github.com/wighawag/hardhat-deploy) by Ronan Sandford.
+
+Deployment scripts perform smart contracts deployment itself and their setup configuration.
+Executing a script may require several transactions to complete, which may fail. To help troubleshoot
+partially finished deployment, the scripts are designed to be rerunnable and execute only the transactions
+which were not executed in previous run(s).
+
+Deployment scripts are located under [deploy](./deploy) folder.
+Deployment execution state is saved under [deployments](./deployments) folder.
+
+To run fresh deployment (<NETWORK_NAME>):
+
+1. Delete [deployments/<NETWORK_NAME>](./deployments/<NETWORK_NAME>) folder contents.
+
+2. Run the deployment of interest with the ```npx hardhat <TAG>``` command
+    ```
+    npx hardhat deploy --network <NETWORK_NAME> --tags <TAG>
+    ```
+    where ```<TAG>``` specifies the deployment script(s) tag to run,
+    and ```--network <NETWORK_NAME>``` specifies the network to run script for
+    (see [hardhat.config.js](./hardhat.config.js) for network definitions).
+
+3. Verify source code on Etherscan with the ```npx hardhat etherscan-verify``` command
+    ```
+    npx hardhat etherscan-verify --network <network_name> --api-key $ETHERSCAN_KEY
+    ```
+
+To rerun the deployment script and continue partially completed script skip the first step
+(do not cleanup the [deployments](./deployments) folder).
---
+++ hardhat.config.js
@@ -0,0 +1,317 @@
+/**
+ * default Hardhat configuration which uses account mnemonic to derive accounts
+ * script expects following environment variables to be set:
+ *   - P_KEY1 – mainnet private key, should start with 0x
+ *     or
+ *   - MNEMONIC1 – mainnet mnemonic, 12 words
+ *
+ *   - P_KEY5003 – mantle sepolia private key, should start with 0x
+ *     or
+ *   - MNEMONIC5003 – mantle sepolia mnemonic, 12 words
+ *
+ *   - P_KEY59141 – linea sepolia private key, should start with 0x
+ *     or
+ *   - MNEMONIC59141 – linea sepolia mnemonic, 12 words
+ *
+ *   - P_KEY1301 – unichain sepolia private key, should start with 0x
+ *     or
+ *   - MNEMONIC1301 – unichain sepolia mnemonic, 12 words
+ *
+ *   - P_KEY48899 – zircuit testnet private key, should start with 0x
+ *     or
+ *   - MNEMONIC48899 – zircuit testnet mnemonic, 12 words
+ *
+ *   - ALCHEMY_KEY – Alchemy API key
+ *     or
+ *   - INFURA_KEY – Infura API key (Project ID)
+ *
+ *   - ETHERSCAN_KEY – Etherscan API key
+ *
+ *   - MANTLESCAN_KEY – Mantlescan API key
+ *
+ *   - LINEASCAN_KEY – LineaScan API key
+ *
+ *   - UNISCAN_KEY – UniScan API key
+ * 
+ * 	 - ZIRCUITSCAN_KEY – ZircuitScan API key	
+ */
+
+// Loads env variables from .env file
+require('dotenv').config()
+
+// Enable Truffle 5 plugin for tests
+// https://hardhat.org/guides/truffle-testing.html
+require("@nomiclabs/hardhat-truffle5");
+
+// enable Solidity-coverage
+// https://hardhat.org/plugins/solidity-coverage.html
+require("solidity-coverage");
+
+// enable hardhat-gas-reporter
+// https://hardhat.org/plugins/hardhat-gas-reporter.html
+// require("hardhat-gas-reporter");
+
+// compile Solidity sources directly from NPM dependencies
+// https://github.com/ItsNickBarry/hardhat-dependency-compiler
+require("hardhat-dependency-compiler");
+
+// copy compiled Solidity bytecode directly from the NPM dependencies.
+// https://github.com/vgorin/hardhat-dependency-injector
+require("hardhat-dependency-injector");
+
+// adds a mechanism to deploy contracts to any network,
+// keeping track of them and replicating the same environment for testing
+// https://www.npmjs.com/package/hardhat-deploy
+require("hardhat-deploy");
+
+// verify environment setup, display warning if required, replace missing values with fakes
+const FAKE_MNEMONIC = "test test test test test test test test test test test junk";
+if(!process.env.MNEMONIC1 && !process.env.P_KEY1) {
+	console.warn("neither MNEMONIC1 nor P_KEY1 is not set. Mainnet deployments won't be available");
+	process.env.MNEMONIC1 = FAKE_MNEMONIC;
+}
+else if(process.env.P_KEY1 && !process.env.P_KEY1.startsWith("0x")) {
+	console.warn("P_KEY1 doesn't start with 0x. Appended 0x");
+	process.env.P_KEY1 = "0x" + process.env.P_KEY1;
+}
+if(!process.env.MNEMONIC5003 && !process.env.P_KEY5003) {
+	console.warn("neither MNEMONIC5003 nor P_KEY5003 is not set. Mantle testnet deployments won't be available");
+	process.env.MNEMONIC3 = FAKE_MNEMONIC;
+}
+else if(process.env.P_KEY5003 && !process.env.P_KEY5003.startsWith("0x")) {
+	console.warn("P_KEY59141 doesn't start with 0x. Appended 0x");
+	process.env.P_KEY5003 = "0x" + process.env.P_KEY5003;
+}
+if(!process.env.MNEMONIC59141 && !process.env.P_KEY59141) {
+	console.warn("neither MNEMONIC59141 nor P_KEY59141 is not set. Linea sepolia deployments won't be available");
+	process.env.MNEMONIC59141 = FAKE_MNEMONIC;
+}
+else if(process.env.P_KEY59141 && !process.env.P_KEY59141.startsWith("0x")) {
+	console.warn("P_KEY59141 doesn't start with 0x. Appended 0x");
+	process.env.P_KEY59141 = "0x" + process.env.P_KEY59141;
+}
+if(!process.env.MNEMONIC1301 && !process.env.P_KEY1301) {
+	console.warn("neither MNEMONIC1301 nor P_KEY1301 is not set. Unichain sepolia deployments won't be available");
+	process.env.MNEMONIC1301 = FAKE_MNEMONIC;
+}
+else if(process.env.P_KEY1301 && !process.env.P_KEY1301.startsWith("0x")) {
+	console.warn("P_KEY1301 doesn't start with 0x. Appended 0x");
+	process.env.P_KEY1301 = "0x" + process.env.P_KEY1301;
+}
+if(!process.env.MNEMONIC48899 && !process.env.P_KEY48899) {
+	console.warn("neither MNEMONIC48899 nor P_KEY48899 is not set. Zircuit testnet deployments won't be available");
+	process.env.MNEMONIC48899 = FAKE_MNEMONIC;
+}
+else if(process.env.P_KEY48899 && !process.env.P_KEY48899.startsWith("0x")) {
+	console.warn("P_KEY48899 doesn't start with 0x. Appended 0x");
+	process.env.P_KEY48899 = "0x" + process.env.P_KEY48899;
+}
+if(!process.env.INFURA_KEY && !process.env.ALCHEMY_KEY) {
+	console.warn("neither INFURA_KEY nor ALCHEMY_KEY is not set. Deployments may not be available");
+	process.env.INFURA_KEY = "";
+	process.env.ALCHEMY_KEY = "";
+}
+if(!process.env.ETHERSCAN_KEY) {
+	console.warn("ETHERSCAN_KEY is not set. Deployed smart contract code verification won't be available");
+	process.env.ETHERSCAN_KEY = "";
+}
+if(!process.env.MANTLESCAN_KEY) {
+	console.warn("MANTLESCAN_KEY is not set. Deployed smart contract code verification won't be available on mantlescan");
+	process.env.MANTLESCAN_KEY = "";
+}
+if(!process.env.LINEASCAN_KEY) {
+	console.warn("LINEASCAN_KEY is not set. Deployed smart contract code verification won't be available on LineaScan");
+	process.env.LINEASCAN_KEY = "";
+}
+if(!process.env.UNISCAN_KEY) {
+	console.warn("UNISCAN_KEY is not set. Deployed smart contract code verification won't be available on UniScan");
+	process.env.LINEASCAN_KEY = "";
+}
+if(!process.env.ZIRCUITSCAN_KEY) {
+	console.warn("ZIRCUITSCAN_KEY is not set. Deployed smart contract code verification won't be available on ZircuitScan");
+	process.env.ZIRCUITSCAN_KEY = "";
+}
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+	defaultNetwork: "hardhat",
+	networks: {
+		// https://hardhat.org/hardhat-network/
+		hardhat: {
+			// set networkId to 0xeeeb04de as for all local networks
+			chainId: 0xeeeb04de,
+			// set the gas price to one for convenient tx costs calculations in tests
+			// gasPrice: 1,
+			// London hard fork fix: impossible to set gas price lower than baseFeePerGas (875,000,000)
+			initialBaseFeePerGas: 0,
+			accounts: {
+				count: 35,
+			},
+/*
+			forking: {
+				url: "https://mainnet.infura.io/v3/" + process.env.INFURA_KEY, // create a key: https://infura.io/
+				enabled: !!(process.env.HARDHAT_FORK),
+			},
+*/
+		},
+		// https://etherscan.io/
+		mainnet: {
+			url: get_endpoint_url("mainnet"),
+			accounts: get_accounts(process.env.P_KEY1, process.env.MNEMONIC1),
+		},
+		// https://sepolia.mantlescan.xyz/
+		mantle: {
+			url: get_endpoint_url("mantle"),
+			accounts: get_accounts(process.env.P_KEY5003, process.env.MNEMONIC5003),
+		},
+		// https://sepolia.lineascan.build/
+		linea: {
+			url: get_endpoint_url("linea"),
+			accounts: get_accounts(process.env.P_KEY59141, process.env.MNEMONIC59141),
+		},
+		// https://sepolia.uniscan.xyz/
+		unichain: {
+			url: get_endpoint_url("unichain"),
+			accounts: get_accounts(process.env.P_KEY1301, process.env.MNEMONIC1301),
+		},
+		// https://explorer.testnet.zircuit.com
+		zircuit: {
+			url: get_endpoint_url("zircuit"),
+			accounts: get_accounts(process.env.P_KEY48899, process.env.MNEMONIC48899),
+		},
+	},
+
+	// Configure Solidity compiler
+	solidity: {
+		// https://hardhat.org/guides/compile-contracts.html
+		compilers: [
+			{ // project main compiler version
+				version: "0.8.4",
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 200
+					}
+				}
+			},
+			{ // used to compile USDT mock
+				version: "0.4.11",
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 200
+					}
+				}
+			},
+		]
+	},
+
+	// Set default mocha options here, use special reporters etc.
+	mocha: {
+		// timeout: 100000,
+
+		// disable mocha timeouts:
+		// https://mochajs.org/api/mocha#enableTimeouts
+		enableTimeouts: false,
+		// https://github.com/mochajs/mocha/issues/3813
+		timeout: false,
+	},
+
+	// hardhat-gas-reporter will be disabled by default, use REPORT_GAS environment variable to enable it
+	// https://hardhat.org/plugins/hardhat-gas-reporter.html
+	gasReporter: {
+		enabled: !!(process.env.REPORT_GAS)
+	},
+
+	// compile Solidity sources directly from NPM dependencies
+	// https://github.com/ItsNickBarry/hardhat-dependency-compiler
+	dependencyCompiler: {
+		paths: [
+			// ERC1967 is used to deploy upgradeable contracts
+			"@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol",
+		],
+	},
+
+	// copy compiled Solidity bytecode directly from NPM dependencies
+	// https://github.com/vgorin/hardhat-dependency-injector
+	dependencyInjector: {
+		paths: [
+			// OwnableToAccessControlAdapter is deployed to facade OZ Ownable contract
+			"@lazy-sol/access-control/artifacts/contracts/OwnableToAccessControlAdapter.sol",
+		],
+	},
+}
+
+/**
+ * Determines a JSON-RPC endpoint to use to connect to the node
+ * based on the requested network name and environment variables set
+ *
+ * Tries to use custom RPC URL first (MAINNET_RPC_URL/ROPSTEN_RPC_URL/RINKEBY_RPC_URL/KOVAN_RPC_URL)
+ * Tries to use alchemy RPC URL next (if ALCHEMY_KEY is set)
+ * Fallbacks to infura RPC URL
+ *
+ * @param network_name one of mainnet/ropsten/rinkeby/kovan
+ * @return JSON-RPC endpoint URL
+ */
+function get_endpoint_url(network_name) {
+	// try custom RPC endpoint first (private node, quicknode, etc.)
+	// create a quicknode key: https://www.quicknode.com/
+	if(process.env.MAINNET_RPC_URL && network_name === "mainnet") {
+		return process.env.MAINNET_RPC_URL;
+	}
+	if(process.env.MANTLE_RPC_URL && network_name === "mantle") {
+		return process.env.MANTLE_RPC_URL;
+	}
+	if(process.env.LINEA_RPC_URL && network_name === "linea") {
+		return process.env.LINEA_RPC_URL;
+	}
+	if(process.env.UNICHAIN_RPC_URL && network_name === "unichain") {
+		return process.env.UNICHAIN_RPC_URL;
+	}
+	if(process.env.ZIRCUIT_RPC_URL && network_name === "zircuit") {
+		return process.env.ZIRCUIT_RPC_URL;
+	}
+
+	// try the alchemy next
+	// create a key: https://www.alchemy.com/
+	if(process.env.ALCHEMY_KEY) {
+		switch(network_name) {
+			case "mainnet": return "https://eth-mainnet.alchemyapi.io/v2/" + process.env.ALCHEMY_KEY;
+		}
+	}
+
+	// fallback to infura
+	// create a key: https://infura.io/
+	if(process.env.INFURA_KEY) {
+		switch(network_name) {
+			case "mainnet": return "https://mainnet.infura.io/v3/" + process.env.INFURA_KEY;
+			case "mantle": return "https://mantle-sepolia.infura.io/v3/" + process.env.INFURA_KEY;
+			case "linea": return "https://linea-sepolia.infura.io/v3/" + process.env.INFURA_KEY;
+		}
+	}
+
+	// some networks don't require API key
+	switch(network_name) {
+		case "mantle": return "https://rpc.sepolia.mantle.xyz";
+		case "linea": return "https://rpc.sepolia.linea.build";
+		case "unichain": return "https://sepolia.unichain.org/";
+		case "zircuit":  return "https://zircuit1-testnet.liquify.com";
+	}
+
+	// fallback to default JSON_RPC_URL (if set)
+	return process.env.JSON_RPC_URL || "";
+}
+
+/**
+ * Depending on which of the inputs are available (private key or mnemonic),
+ * constructs an account object for use in the hardhat config
+ *
+ * @param p_key account private key, export private key from mnemonic: https://metamask.io/
+ * @param mnemonic 12 words mnemonic, create 12 words: https://metamask.io/
+ * @return either [p_key] if p_key is defined, or {mnemonic} if mnemonic is defined
+ */
+function get_accounts(p_key, mnemonic) {
+	return p_key? [p_key]: mnemonic? {mnemonic, initialIndex: 0}: undefined;
+}
